9a03df758bd4843602a99c5163b80be07622dd10
maxtkarlsson <max.karlsson93@gmail.com>
2022-11-30 13:52:23 +0100

    added some more tests

--- src/__test__/movieApp.test.ts
+++ src/__test__/movieApp.test.ts
@@ -1,7 +1,10 @@
 /**
  * @jest-environment jsdom
  */
+import { IMovie } from "../ts/models/IMovie";
 import * as functions from "./../ts/movieApp";
+import * as functionsServices from "./../ts/services/movieService";
+jest.mock("./../ts/services/movieService.ts");
 
 describe("init", () => {
   test("should call handleSubmit if clicked", () => {
@@ -23,5 +26,42 @@ describe("init", () => {
 
     //Assert
     expect(spy).toHaveBeenCalled();
+  });
+});
+
+describe("createHtml", () => {
+  test("should create html from a list and add class", async () => {
+    //Arrange
+    let text = "text";
+    let movies: IMovie[] = await functionsServices.getData(text);
+    document.body.innerHTML = `
+    <div id="movie-container"></div>`;
+    let container: HTMLDivElement = document.getElementById(
+      "movie-container"
+    ) as HTMLDivElement;
+
+    //Act
+    functions.createHtml(movies, container);
+    //Assert
+    expect(document.querySelectorAll("h3").length).toBe(4);
+    expect(document.querySelectorAll("h3")[0].innerHTML).toContain("Lotr");
+    expect(document.querySelectorAll("img").length).toBe(4);
+    expect(document.querySelectorAll("div.movie").length).toBe(4);
+  });
+});
+
+describe("displayNoResult", () => {
+  test("should create html with text", () => {
+    //Arrange
+    document.body.innerHTML = `<div id="movie-container"></div>`;
+    let container: HTMLDivElement = document.getElementById(
+      "movie-container"
+    ) as HTMLDivElement;
+
+    //Act
+    functions.displayNoResult(container);
+
+    //Assert
+    expect(document.querySelectorAll("p").length).toBe(1);
   });
 });
